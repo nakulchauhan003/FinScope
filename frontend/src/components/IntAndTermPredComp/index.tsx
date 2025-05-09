1753bece626ec7a5da6906f52f66b05d0c0aa873
Nakul Chauhan <nakulchauhan300@gmail.com>
2025-05-09 19:56:35 +0530

    Update index.tsx

--- frontend/src/components/IntAndTermPredComp/index.tsx
+++ frontend/src/components/IntAndTermPredComp/index.tsx
@@ -1,7 +1,6 @@
-import { p } from 'framer-motion/client'
 import InterestPrediction from '../model/InterestPrediction'
 export default function IntAndTermPredComponent(){
     return(
-        p
+        <InterestPrediction/>
     )
 }
