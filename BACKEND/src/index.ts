170f47d424db5349b492ce88af17b2a7ae969e42
Aditya Kumar <aditya227192@gmail.com>
2025-05-14 16:52:18 +0530

    Fixed Issues

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -1,16 +1,15 @@
 import express,{Request,Response} from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({path:"../.env"});
 
 const app=express();
 
 const PORT= process.env.PORT || 9090;
-
 app.get('/',(req,res)=>{
-    res.send('SERVER IS RUNNING');
+    res.json({message:'SERVER IS RUNNING'});
 })
 
 app.listen(PORT,()=>{
-    console.log(`Server Served At Port ${PORT}`);
+    console.log(`Server Served At PORT ${PORT}`);
 })
 app.use(express.json());
